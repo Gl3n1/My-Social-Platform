8072a339a845277d4c82f264e9d23433886b9a9b
gleen <gleen.lexry@n2nconnect.com>
2019-01-16 22:25:03 +0800

    successfully connected to DB

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//DB Config
+
+const db = require('./config/keys').mongoURI;
+
+//connect to DB
+
+mongoose
+  .connect(db)
+  .then(()=> console.log("you're connect to the DB"))
+  .catch(err => console.log(`oh no, error!`))
+
 
 app.get('/', (req,res)=> res.send('Hello World'));
 
