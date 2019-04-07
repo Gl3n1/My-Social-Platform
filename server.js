a50df79dc414b777a834fc397f13030929539555
Glen <glenlexry@seekasia.com>
2019-04-07 15:27:16 +0800

    setup routes

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const users = require('./routes/api/posts');
+const profiles = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 //DB Config
 
 const db = require('./config/keys').mongoURI;
@@ -12,10 +16,15 @@ const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db)
   .then(()=> console.log("you're connect to the DB"))
-  .catch(err => console.log(`oh no, error!`))
+  .catch(err => console.log(`oh no, error connecting to DB !`))
 
 
 app.get('/', (req,res)=> res.send('Hello World'));
+
+//use routes 
+app.use('/api/users', users)
+app.use('/api/profile', profiles)
+app.use('/api/posts', posts)
 
 const port = process.env.PORT || 5000;
 
